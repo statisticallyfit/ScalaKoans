595d04d232d484b2eed99ad52237d8d877378efe
BabitzdaBrave <amg.vintila@gmail.com>
2019-03-08 11:10:36 -0500

    did beautiful Amphibian croaks example in chapter 4 for koans

--- src/test/scala/org/functionalkoans/forscala/AboutConstructors.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutConstructors.scala
@@ -2,6 +2,8 @@ package org.functionalkoans.forscala
 
 import support.KoanSuite
 
+import scala.collection.mutable.ListBuffer
+
 class AboutConstructors extends KoanSuite {
 
 
@@ -152,5 +154,160 @@ class AboutConstructors extends KoanSuite {
 
           //not available
           //stock.currentPrice
+     }
+
+     koan("A private field can be seen by any instance with the private field. Any other" +
+          "instance of the same type can access its own private field, too"){
+
+          class Stock{
+               //no getters or setters are generated
+               private var price: Double = _
+
+               def setPrice(p: Double) { price = p}
+               def isHigher(that: Stock): Boolean = this.price > that.price
+          }
+
+          val s1 = new Stock
+          s1.setPrice(20)
+          val s2 = new Stock
+          s2.setPrice(100)
+
+          s2.isHigher(s1) should be(true)
+     }
+
+
+     koan("An object-private field can only be accessed from the object that contains it" +
+          "so not even other objects of the same type can see that field. This makes" +
+          "the field more private than the plain private setting."){
+
+          class MusicalInstrument {
+               //a private[this] var is object-private and can only be seen by the
+               //current instance
+               private[this] var tonality: Double = _
+
+               def setTune(t: Int) {tonality = t }
+
+               //error: this method won't compile because tonality is now object-private
+               //def isHigher(that: MusicalInstrument): Boolean = this.tonality > that.tonality
+          }
+     }
+
+
+     koan("Subclasses can control the superclass constructor, because it is called by" +
+          " the primary constructor of the subclass"){
+
+          // (1) primary constructor
+          class WildCat(var name: String, var age: Int) {
+
+               var roars: ListBuffer[String] = ListBuffer()
+               roars += "Wildcat primary"
+
+               // (2) auxiliary constructor
+               def this(name: String) {
+                    this(name, 0)
+                    roars += "Wildcat auxiliary"
+               }
+
+               override def toString = s"$name is $age years old"
+          }
+
+          //calls the Wildcat auxiliary (this) constructor since only name is passed
+          class Leopard(name: String, var numSpots: Int) extends WildCat(name) {
+               //now the leopard primary constructor is called
+               roars += "Leopard primary"
+          }
+
+          //calls the Wildcat primary constructor since both name and age are passed
+          class BengalTiger(name: String, var numStripes: Int, age: Int) extends WildCat(name, age){
+               roars += "BengalTiger primary"
+          }
+
+          val leo = new Leopard("Leo", 23)
+          leo.age should be(0)
+          leo.roars should be(Seq("Wildcat primary", "Wildcat auxiliary", "Leopard primary"))
+
+          val tigger = new BengalTiger("Tigger", 10, 5)
+          tigger.age should be(5)
+          tigger.roars should be(Seq("Wildcat primary", "BengalTiger primary"))
+     }
+
+
+     koan("Auxiliary constructors of a class are limited to calling other " +
+          "constructors within that same class, never reaching superclass constructors."){
+
+          trait Environment
+          case object Marsh extends Environment
+          case object Jungle extends Environment
+
+          trait Hemisphere
+          case object SouthernHemi extends Hemisphere
+          case object NorthernHemi extends Hemisphere
+
+          trait Color
+          case object Red extends Color
+          case object Green extends Color
+          case object Brown extends Color
+
+
+          case class Habitat(hemisphere: Hemisphere, marshOrJungle: Environment) //true if marsh
+          case class Poison(level: Int)
+
+          class Amphibian(var color: Color, var habitat: Habitat) {
+
+               //testing how many times the primary constructor and auxiliary constructors are reached
+               var croaks: ListBuffer[String] = ListBuffer("Amphibian primary")
+
+               // no way for the Frog auxiliary constructors to call this
+               // auxiliary constructor
+               def this (color: Color) {
+                    this(color, null) //calls the primary constructor for Amphibian
+                    //habitat
+                    croaks += "Amphibian auxiliary"
+               }
+
+               override def toString = if (habitat == null) color.toString else s"$color @ $habitat"
+          }
+
+          class Frog(color: Color, var level: Poison, habitat: Habitat)
+               extends Amphibian(color, habitat){
+
+               croaks += "Frog primary"
+
+               def this(color:Color){
+                    this(color, null, null) //calls the Frog auxiliary constructor,
+                    //can never reach the Amphibian auxiliary constructor above
+                    croaks += "Frog auxiliary Color"
+               }
+
+               def this(color: Color, level: Poison) {
+                    this(color, level, null)
+
+                    croaks += "Frog auxiliary Color Poison"
+               }
+
+               def this(color: Color, habitat: Habitat) {
+                    this(color, null, habitat)
+
+                    croaks += "Frog auxiliary Color Habitat"
+               }
+          }
+
+          //Main point: no direct way for subclass to control which superclass constructor
+          //is called from the subclass's auxiliary constructor.
+          // Each auxiliary constructor is limited to calling a previously defined
+          // constructor in the same class.
+
+          val amph = new Amphibian(Green, Habitat(NorthernHemi, Marsh))
+
+          val marshFrog = new Frog(Brown, Poison(0), Habitat(NorthernHemi, Marsh))
+          val brightJungleFrog = new Frog(Red, Habitat(SouthernHemi, Jungle))
+          brightJungleFrog.level = Poison(9)
+          val unknownFrog = new Frog(Green, Poison(7))
+
+          marshFrog.croaks should be(List("Amphibian primary", "Frog primary"))
+          brightJungleFrog.croaks should be(List("Amphibian primary", "Frog primary", "Frog auxiliary Color Habitat"))
+          unknownFrog.croaks should be(List("Amphibian primary", "Frog primary", "Frog auxiliary Color Poison"))
+
+
      }
 }
--- src/test/scala/org/functionalkoans/forscala/AboutParentClasses.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutParentClasses.scala
@@ -15,15 +15,28 @@ class AboutParentClasses extends KoanSuite {
      }
 
      koan("A class that extends from another is polymorphic") {
-          class Worker(val firstName: String, val lastName: String) {}
-          class Employee(override val firstName: String, override val lastName: String,
-                         val employeeID: Long) extends Worker(firstName, lastName)
 
-          val me = new Employee("Name", "Yourself", 1233)
+          //Note: the arguments that are the same in the subclass can be declared in subclass either
+          // plainly (like firstName) or with 'override val' (for lastName).
+          // New arguments don't have to take a keyword unless you want it to be private, as usual
+          class Worker(val firstName: String, val lastName: String) {}
+          class Employee(firstName: String, override val lastName: String,
+                         employeeID: Long, var employeeRating: Int) extends Worker(firstName, lastName)
+
+          val me = new Employee("Name", "Yourself", 1233, 10)
           val worker: Worker = me
 
           worker.firstName should be("Name")
           worker.lastName should be("Yourself")
+
+          me.employeeRating = 9 //var args can be changed and accessed as uaual
+          me.employeeRating should be(9)
+
+          me.firstName should be("Name") //the subclass retains the superclass getter/setter even if the subclass
+          // declares that field private
+          me.lastName should be("Yourself")
+
+          // me.employeeID // this doesn't work, is private as usual
      }
 
      koan("An abstract class, as in Java, cannot be instantiated and only inherited") {
